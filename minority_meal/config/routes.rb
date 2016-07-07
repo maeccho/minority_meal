83db0f0f9c35fd0bbf586a028c5e5c46b935cebb
Kazuya Maekawa <kazifyify@gmail.com>
2016-07-07 21:51:05 +0900

    add create & new act

--- minority_meal/config/routes.rb
+++ minority_meal/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "top#index"
   get 'tweets' => 'tweets#index'
+  get 'tweets/new' => 'tweets#new'
+  post 'tweets' => 'tweets#create'
 
 
 
