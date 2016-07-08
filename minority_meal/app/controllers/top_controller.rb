9aa74fee40dbcc2cc6feff5f98d22867d1194cb2
Kazuya Maekawa <kazifyify@gmail.com>
2016-07-08 19:47:13 +0900

    add login fun and etc.

--- minority_meal/app/controllers/top_controller.rb
+++
@@ -1,6 +0,0 @@
-class TopController < ApplicationController
-
-  def index
-    @tweets = Tweet.all
-  end
-end
--- minority_meal/app/controllers/tweets_controller.rb
+++ minority_meal/app/controllers/tweets_controller.rb
@@ -1,14 +1,25 @@
 class TweetsController < ApplicationController
 
+  before_action :move_to_index, except: :index
+
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order("id DESC")
   end
 
   def new
   end
 
   def create
-    Tweet.create(name: params[:name], shop_name: params[:shop_name], explanation: params[:explanation]), address: params[:address] )
+    Tweet.create(name: tweet_params[:name], shop_name: tweet_params[:shop_name], image: tweet_params[:image], explanation: tweet_params[:explanation], address: tweet_params[:address], user_id: current_user.id)
+  end
+
+  private
+  def tweet_params
+    params.permit(:name, :shop_name, :image, :explanation, :address)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
 end
